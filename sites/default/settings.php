8953f92a7f3cf80efc25de188a071b62d7ab7ca8
Thomas SERRES <contact@thomas-serres.fr>
2021-10-18 14:41:33 +0200

    drupal base

---
+++ sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'serres_info',
+  'username' => 'serres_info',
+  'password' => 'Clm2pMySQL.',
+  'prefix' => 'si_',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'FQkAmgUE01Xy46cV5GaorIjMYpoQQAkYIiP6GM82oQXG_TxHoOveTgQ_bLfPnFBUggQBrn8dlg';
+$settings['config_sync_directory'] = 'config/sync';
